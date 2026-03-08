0075f3a4ba837f98c27c50da24dead98663f8809
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2026-03-07 18:52:42 -0800

    chore: update NVIDIA copyright headers through 2026

--- internal/pkg/module/module.go
+++ internal/pkg/module/module.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2023-2026, NVIDIA CORPORATION. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
